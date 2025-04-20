83519cdf0644f2e449931fb8ab7359ea1951afba
Jordan Bentley <jordan42293@gmail.com>
2025-04-20 09:59:06 -0400

    init commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "BookWyrm" },
+    { name: "description", content: "Welcome to BookWyrm!" },
   ];
 }
 
